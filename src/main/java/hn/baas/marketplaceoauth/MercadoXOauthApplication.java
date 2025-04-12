7d4e7e6ac9209672d02dbf55f3240002ccc7a60d
Igor A. Zelaya <izelaya@baas.hn>
2025-04-12 11:30:47 -0600

    main: Initial Commit

---
+++ src/main/java/hn/baas/marketplaceoauth/MercadoXOauthApplication.java
@@ -0,0 +1,13 @@
+package hn.baas.marketplaceoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoXOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercadoXOauthApplication.class, args);
+    }
+
+}
