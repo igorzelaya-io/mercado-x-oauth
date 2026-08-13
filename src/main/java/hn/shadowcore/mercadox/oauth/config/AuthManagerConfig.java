3c5ac23b793d6b707d0ff0973bc79e8324009ccf
Igor A. Zelaya <izelaya22@gmail.com>
2026-08-13 13:48:38 -0600

    develop: added conditional on bean for private key signing isolation

--- src/main/java/hn/shadowcore/mercadox/oauth/config/AuthManagerConfig.java
+++ src/main/java/hn/shadowcore/mercadox/oauth/config/AuthManagerConfig.java
@@ -1,5 +1,6 @@
 package hn.shadowcore.mercadox.oauth.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Import(AuthenticationConfiguration.class)
+@ConditionalOnProperty(name = "security.jwt.private-key-location")
 public class AuthManagerConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
